b990ff60086b9b68fb7eb3303c8817dc13d4ffec
lsfwx2 <lsfwx2@mail.missouri.edu>
2016-11-13 16:38:20 -0600

    changed some more things
    still to change: look into allowing pilot_flight table to check whether the scheduled pilot is certified to fly the scheduled plane (or can this only be done in php?) among other things.

--- project.sql
+++ project.sql
@@ -1,139 +1,3 @@
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-`user_id` int(10) NOT NULL AUTO_INCREMENT,
-`fname` varchar(15),
-`lname` varchar(15),
-PRIMARY KEY (`user_id`)
-);
-
-DROP TABLE IF EXISTS `log`;
-CREATE TABLE `log` (
-`log_id` int(10) NOT NULL AUTO_INCREMENT,
-`ip_address` varchar(10) NOT NULL,
-`action_date` date NOT NULL,
-`action` varchar(50) NOT NULL,
-`user_id` int(10) NOT NULL,
-PRIMARY KEY(`log_id`),
-FOREIGN KEY(`user_id`) REFERENCES `user` (`user_id`)
-);
-
-DROP TABLE IF EXISTS `employee`;
-CREATE TABLE `employee` (
-`employee_id` int(10) NOT NULL,
-`role` varchar(15),
-PRIMARY KEY(`employee_id`),
-FOREIGN KEY (`employee_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS `authentication`;
-CREATE TABLE `authentication`(
-`employee_id` int(10) NOT NULL,
-`password` varchar(30) NOT NULL,
-`role` varchar(15),
-PRIMARY KEY(`employee_id`),
-FOREIGN KEY(`employee_id`) REFERENCES `employee` (`employee_id`)  ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS `customer`;
-CREATE TABLE `customer` (
-`customer_id` int(10) NOT NULL,
-`age` int(3) NOT NULL,
-PRIMARY KEY (`customer_id`),
-FOREIGN KEY (`customer_id`) REFERENCES `user` (`user_id`)  ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS `administrator`;
-CREATE TABLE `administrator`(
-`admin_id` int(10) NOT NULL,
-`admin_rank` varchar(10),
-PRIMARY KEY (`admin_id`),
-FOREIGN KEY (`admin_id`) REFERENCES `employee` (`employee_id`)  ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS `pilot`;
-CREATE TABLE `pilot` (
-`pilot_id` int(10) NOT NULL,
-`status` varchar(10) NOT NULL,
-`flight_hours` int(10) NOT NULL,
-CHECK (`flight_hours` > 0),
-`pilot_rank` varchar(10) NOT NULL,
-PRIMARY KEY (`pilot_id`),
-FOREIGN KEY (`pilot_id`) REFERENCES `employee` (`employee_id`)  ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS `attendant`;
-CREATE TABLE `attendant` (
-`attendant_id` int(10) NOT NULL,
-`attendant_rank` varchar(10) NOT NULL,
-PRIMARY KEY (`attendant_id`),
-FOREIGN KEY (`attendant_id`) REFERENCES `employee` (`employee_id`)  ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS `model`;
-CREATE TABLE `model`(
-`plane_model` varchar(15) NOT NULL,
-`num_pilots` int(2) NOT NULL,
-`num_attendants` int(2) NOT NULL,
-`num_passengers` int(4) NOT NULL,
-PRIMARY KEY (`plane_model`)
-);
-
-DROP TABLE IF EXISTS `equipment`;
-CREATE TABLE `equipment`(
-`plane_id` int(10) NOT NULL AUTO_INCREMENT,
-`plane_model` varchar(15) NOT NULL,
-PRIMARY KEY(`plane_id`),
-FOREIGN KEY(`plane_model`) REFERENCES `model` (`plane_model`)
-);
-
-DROP TABLE IF EXISTS `flight`;
-CREATE TABLE `flight`(
-`flight_id` int(10) NOT NULL AUTO_INCREMENT,
-`departing_city` varchar(30) NOT NULL,
-`destination_city` varchar(30) NOT NULL,
-`plane_id` int(10) NOT NULL,
-`days` varchar(10) NOT NULL,
-PRIMARY KEY (`flight_id`),
-FOREIGN KEY (`plane_id`) REFERENCES `equipment` (`plane_id`)
-);
-
-DROP TABLE IF EXISTS `reservation`;
-CREATE TABLE `reservation`(
-`flight_id` int(10) NOT NULL,
-`customer_id` int(10) NOT NULL,
-`date_reserved` date NOT NULL,
-`num_bags` int(2) NOT NULL,
-PRIMARY KEY (`flight_id`, `customer_id`),
-FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON DELETE CASCADE,
-FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS `pilot_model`;
-CREATE TABLE `pilot_model`(
-`pilot_id` int(10) NOT NULL,
-`plane_model` varchar(15) NOT NULL,
-PRIMARY KEY (`pilot_id`, `plane_model`),
-FOREIGN KEY (`pilot_id`) REFERENCES `pilot` (`pilot_id`) ON DELETE CASCADE,
-FOREIGN KEY (`plane_model`) REFERENCES `model`(`plane_model`) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS `pilot_flight`;
-CREATE TABLE `pilot_flight` (
-`pilot_id` int(10) NOT NULL,
-`flight_id` int(10) NOT NULL,
-PRIMARY KEY (`pilot_id`, `flight_id`),
-FOREIGN KEY (`pilot_id`) REFERENCES `pilot` (`pilot_id`) ON DELETE CASCADE,
-FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS `attendant_flight`;
-CREATE TABLE `attendant_flight`(
-`attendant_id` int(10) NOT NULL,
-`flight_id` int(10) NOT NULL,
-PRIMARY KEY (`attendant_id`, `flight_id`),
-FOREIGN KEY (`attendant_id`) REFERENCES `attendant` (`attendant_id`) ON DELETE CASCADE,
-FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON DELETE CASCADE
-);
 
 --To drop all tables, execute the following in this order:
 
@@ -146,32 +10,156 @@ DROP TABLE IF EXISTS `equipment`;
 DROP TABLE IF EXISTS `model`;
 DROP TABLE IF EXISTS `attendant`;
 DROP TABLE IF EXISTS `pilot`;
-DROP TABLE IF EXISTS `administrator`;
+DROP TABLE IF EXISTS `admin`;
 DROP TABLE IF EXISTS `customer`;
 DROP TABLE IF EXISTS `authentication`;
 DROP TABLE IF EXISTS `employee`;
 DROP TABLE IF EXISTS `log`;
 DROP TABLE IF EXISTS `user`;
 
+CREATE TABLE `user` (
+`user_id` int(10) NOT NULL AUTO_INCREMENT,
+`fname` varchar(15) NOT NULL,
+`lname` varchar(15) NOT NULL,
+`role` ENUM('admin', 'pilot', 'attendant', 'customer') NOT NULL DEFAULT 'customer',
+PRIMARY KEY (`user_id`)
+);
+
+CREATE TABLE `log` (
+`log_id` int(10) NOT NULL AUTO_INCREMENT,
+`ip_address` varchar(10) NOT NULL,
+`action_date` date NOT NULL,
+`action` varchar(50) NOT NULL,
+`user_id` int(10) NOT NULL,
+PRIMARY KEY(`log_id`),
+FOREIGN KEY(`user_id`) REFERENCES `user` (`user_id`)
+);
+
+CREATE TABLE `employee` (
+`employee_id` int(10) NOT NULL,
+PRIMARY KEY(`employee_id`),
+FOREIGN KEY (`employee_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `authentication`(
+`user_id` int(10) NOT NULL,
+`password` varchar(30) NOT NULL,
+PRIMARY KEY(`user_id`),
+FOREIGN KEY(`user_id`) REFERENCES `user` (`user_id`)  ON DELETE CASCADE
+);
+
+CREATE TABLE `customer` (
+`customer_id` int(10) NOT NULL,
+`age` int(3) NOT NULL,
+PRIMARY KEY (`customer_id`),
+FOREIGN KEY (`customer_id`) REFERENCES `user` (`user_id`)  ON DELETE CASCADE
+);
+
+CREATE TABLE `admin`(
+`admin_id` int(10) NOT NULL,
+PRIMARY KEY (`admin_id`),
+FOREIGN KEY (`admin_id`) REFERENCES `employee` (`employee_id`)  ON DELETE CASCADE
+);
+
+CREATE TABLE `pilot` (
+`pilot_id` int(10) NOT NULL,
+`status` ENUM('active', 'inactive') NOT NULL,
+`flight_hours` int(10) NOT NULL,
+CHECK (`flight_hours` > 0),
+`pilot_rank` ENUM('pilot', 'copilot') NOT NULL,
+PRIMARY KEY (`pilot_id`),
+FOREIGN KEY (`pilot_id`) REFERENCES `employee` (`employee_id`)  ON DELETE CASCADE
+);
+
+CREATE TABLE `attendant` (
+`attendant_id` int(10) NOT NULL,
+`attendant_rank` ENUM('senior', 'junior') NOT NULL,
+PRIMARY KEY (`attendant_id`),
+FOREIGN KEY (`attendant_id`) REFERENCES `employee` (`employee_id`)  ON DELETE CASCADE
+);
+
+CREATE TABLE `model`(
+`plane_model` varchar(15) NOT NULL,
+`num_pilots` int(2) NOT NULL,
+`num_attendants` int(2) NOT NULL,
+`num_passengers` int(4) NOT NULL,
+PRIMARY KEY (`plane_model`)
+);
+
+CREATE TABLE `equipment`(
+`plane_id` int(10) NOT NULL AUTO_INCREMENT,
+`plane_model` varchar(15) NOT NULL,
+PRIMARY KEY(`plane_id`),
+FOREIGN KEY(`plane_model`) REFERENCES `model` (`plane_model`)
+);
+
+CREATE TABLE `flight`(
+`flight_id` int(10) NOT NULL AUTO_INCREMENT,
+`departing_city` varchar(30) NOT NULL,
+`destination_city` varchar(30) NOT NULL,
+`plane_id` int(10) NOT NULL,
+`days` varchar(10) NOT NULL,
+CONSTRAINT no_circle CHECK (`destination_city` != `departing_city`),
+PRIMARY KEY (`flight_id`),
+FOREIGN KEY (`plane_id`) REFERENCES `equipment` (`plane_id`)
+);
+
+CREATE TABLE `reservation`(
+`flight_id` int(10) NOT NULL,
+`customer_id` int(10) NOT NULL,
+`date_reserved` date NOT NULL,
+`num_bags` int(2) NOT NULL,
+PRIMARY KEY (`flight_id`, `customer_id`),
+FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON DELETE CASCADE,
+FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `pilot_model`(
+`pilot_id` int(10) NOT NULL,
+`plane_model` varchar(15) NOT NULL,
+PRIMARY KEY (`pilot_id`, `plane_model`),
+FOREIGN KEY (`pilot_id`) REFERENCES `pilot` (`pilot_id`) ON DELETE CASCADE,
+FOREIGN KEY (`plane_model`) REFERENCES `model`(`plane_model`) ON DELETE CASCADE
+);
+
+CREATE TABLE `pilot_flight` (
+`pilot_id` int(10) NOT NULL,
+`flight_id` int(10) NOT NULL,
+PRIMARY KEY (`pilot_id`, `flight_id`),
+FOREIGN KEY (`pilot_id`) REFERENCES `pilot` (`pilot_id`) ON DELETE CASCADE,
+FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `attendant_flight`(
+`attendant_id` int(10) NOT NULL,
+`flight_id` int(10) NOT NULL,
+PRIMARY KEY (`attendant_id`, `flight_id`),
+FOREIGN KEY (`attendant_id`) REFERENCES `attendant` (`attendant_id`) ON DELETE CASCADE,
+FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON DELETE CASCADE
+);
+
+
 --   --   --   --   --   --   --   --   --   --   --   --   --   --   --   --
 
--------------------------------------
---creating new employee from internet site UI:
-/*employee:
-	Name: John Smith
-	Role: Pilot
-	Status: Active
-	Flight Hours: 438
-	Rank: Pilot
-*/
-
-INSERT INTO `user` (`fname`, `lname`) VALUES ('John', 'Smith');
-$id = SELECT `user_id` FROM `user` WHERE `fname`='John' AND `lname`='Smith';
-INSERT INTO `empoloyee`(`employee_id`, `role`) VALUES ($id, 'Pilot');
-INSERT INTO `pilot` (`pilot_id`, `status`, `flight_hours`, `pilot_rank`) VALUES($id, 'Active', 438, 'Pilot');
-
--------------------------------------
-
+/* OLD
 SELECT reservation.flight_id, departing_city, destination_city, fname, lname, num_bags*20 AS price FROM flight, reservation, user, customer WHERE user_id = customer.customer_id AND customer.customer_id = reservation.customer_id AND reservation.flight_id = flight.flight_id GROUP BY flight.flight_id;
 
 SELECT flight.flight_id, departing_city, destination_city, COUNT(reservation.flight_id) AS Passengers, SUM(reservation.num_bags*20) AS Revenue FROM flight, reservation WHERE reservation.flight_id = flight.flight_id;
+*/
+
+INSERT INTO `user` (`fname`, `lname`, `role`) VALUES ('John', 'Doe', 'pilot'), ('Jane', 'Smith', 'Admin'), ('Gus', 'Tomer', 'customer'), ('Ann', 'Smiley', 'attendant');
+INSERT INTO `employee` (`employee_id`) VALUES (1), (2), (4);
+INSERT INTO `customer` (`customer_id`, `age`) VALUES (3, 20);
+INSERT INTO `admin` (`admin_id`) VALUES (2);
+INSERT INTO `pilot` (`pilot_id`, `status`, `flight_hours`, `pilot_rank`) VALUES (1, 'active', 104, 'pilot');
+INSERT INTO `attendant` (`attendant_id`, `attendant_rank`) VALUES (4, 'senior');
+INSERT INTO `authentication` (`user_id`, `password`) VALUES (1, 'PilotPass'), (2, 'AdminPass'), (4, 'AttPass');
+INSERT INTO `model` (`plane_model`, `num_pilots`, `num_attendants`, `num_passengers`) VALUES ('Denali', 2, 1, 6), ('Longitude', 2, 1, 8);
+INSERT INTO `equipment` (`plane_model`) VALUES ('Denali'), ('Denali'), ('Denali'), ('Longitude'), ('Longitude');
+INSERT INTO `flight` (`departing_city`, `destination_city`, `plane_id`, `days`) VALUES ('Columbia', 'KansasCity', 2, 'Monday');
+INSERT INTO `pilot_flight` (`pilot_id`, `flight_id`) VALUES (1, 1);
+INSERT INTO `pilot_model` (`pilot_id`, `plane_model`) VALUES (1, 'Denali');
+INSERT INTO `attendant_flight` (`attendant_id`, `flight_id`) VALUES (4, 1);
+INSERT INTO `reservation` (`flight_id`, `customer_id`, `date_reserved`, `num_bags`) VALUES (1, 3, '2016-04-30', 4);
+
+select fname, lname, departing_city, destination_city, plane_model from user, flight, pilot_flight, equipment where user_id=pilot_flight.pilot_id and pilot_flight.flight_id=flight.flight_id and equipment.plane_id=flight.plane_id;
