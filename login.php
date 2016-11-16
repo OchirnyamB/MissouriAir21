d1f2d956ccfa1fb96215d6154338bbfaad23302e
OchirnyamB <ochirnyam@gmail.com>
2016-11-15 23:28:34 -0600

    Added multiple frond end pages, and pre-complete website flow

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Employee Portal Login Website</title>
+        <title>User Login Page</title>
         <!-- CSS -->
         <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
         <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
@@ -19,7 +19,7 @@
              <div class="container">
                     <div class="row">
                         <div class="col-sm-8 col-sm-offset-2 text">
-                            <h1><strong>Employee</strong> Portal Login</h1>
+                            <h1><strong>User</strong> Login</h1>
                             <div class="description">
                             </div>
                         </div>
@@ -28,15 +28,15 @@
                         <div class="col-sm-6 col-sm-offset-3 form-box">
                         	<div class="form-top">
                         		<div class="form-top-left">
-                            		<h3> Only Employees or Admins</h3>
-                                        <p>Enter your username and password to log in:</p>
+                            		<h3> Please select user type: </h3>
+                                    <p> Enter your username and password to log in! </p>
                         		</div>
                         		<div class="form-top-right">
                         			<i class="fa fa-suitcase"></i>
                         		</div>
                                </div>
                                <div class="form-bottom">
-			                    <form role="form" action="login.php" method="post" class="login-form">
+			                    <form role="form" action="customer.php" method="post" class="login-form">
 			                    	<div class="form-group">
 			                    		<label class="sr-only" for="form-username">Username</label>
 			                        	<input type="text" name="form-username" placeholder="Username..." class="form-username form-control" id="form-username">
@@ -45,7 +45,9 @@
 			                        	<label class="sr-only" for="form-password">Password</label>
 			                        	<input type="password" name="form-password" placeholder="Password..." class="form-password form-control" id="form-password">
 			                        </div>
-			                        <button type="submit" name="login" class="btn btn-success">Login!</button>
+									<label class="checkbox-inline"><input type="checkbox" value="customer" checked>Customer</label>
+									<label class="checkbox-inline"><input type="checkbox" value="employee">Employee</label>
+				                    <button type="submit" name="login" class="btn btn-success">Login!</button>
 			                    </form>
 								<!--<form role="form" action="register.php" method="post" class="register-form">
 									<button type="submit" name="register" class="btn btn-info">Register!</button>
--- show_flights.php
+++ show_flights.php
@@ -1,1 +1,166 @@
-<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Generated Flights</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Missouri Airlines</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="book_now.php">Back</a></li>
+      </ul>
+      <form class="navbar-form navbar-right" role="search">
+        <div class="form-group input-group">
+          <input type="text" class="form-control" placeholder="Search..">
+          <span class="input-group-btn">
+            <button class="btn btn-default" type="button">
+              <span class="glyphicon glyphicon-search"></span>
+            </button>
+          </span>
+        </div>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div class="container text-center">
+  <div class="row">
+    <div class="col-sm-3 well">
+      <div class="well">
+        <p><a href="#">Your selected criteria of all the available flights</a></p>
+      </div>
+      <div class="well">
+        <p><a href="#">Sort By:</a></p>
+        <p>
+		  <span class="btn btn-info">Destination</span><br><br>
+          <span class="btn btn-primary">Price</span><br><br>
+          <span class="btn btn-success">Day/Time</span>
+        </p>
+      </div>
+      <div class="alert alert-success fade in">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+        <p><strong>Reminder!</strong></p>
+        When you select a reservation later, you must enter first name, last  name, age and number of bags!
+      </div>
+	  <div class="well">
+		<p><a href="book_now.php">Go Back</a></p>
+		<p><a href="index.html">Go to Home Page</a></p>
+	  </div>
+    </div>
+    <div class="col-sm-7">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="panel panel-default text-left">
+            <div class="panel-body">
+              <h3>Available Flights Today:</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-9">
+          <div class="well">
+            <p>Columbia Missouri-Saint Louis Missouri</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+		    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		  </div>
+        </div>
+	    <div class="col-sm-3">
+           <div class="well">
+				<p>Flight Details:</p>
+				<!--<img src="paris.jpg" alt="Paris" width="400" height="300"> -->
+					<p><span class="glyphicon glyphicon-calendar"></span> July 21st</p>
+					<p><span class="glyphicon glyphicon-tag"></span> 3 bag limit</p>
+					<p><span class="glyphicon glyphicon-time"></span> 6hr flight</p>
+					<p><span class="glyphicon glyphicon-usd"></span> 299.99</p>
+				<button class="btn btn-primary">Book Now</button>
+		    </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-9">
+          <div class="well">
+            <p>Columbia Missouri-Saint Louis Missouri</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+		    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		  </div>
+        </div>
+	    <div class="col-sm-3">
+           <div class="well">
+				<p>Flight Details:</p>
+				<!--<img src="paris.jpg" alt="Paris" width="400" height="300"> -->
+					<p><span class="glyphicon glyphicon-calendar"></span> July 21st</p>
+					<p><span class="glyphicon glyphicon-tag"></span> 3 bag limit</p>
+					<p><span class="glyphicon glyphicon-time"></span> 6hr flight</p>
+					<p><span class="glyphicon glyphicon-usd"></span> 299.99</p>
+				<button class="btn btn-primary">Book Now</button>
+		    </div>
+        </div>
+      </div>
+	  <div class="row">
+        <div class="col-sm-9">
+          <div class="well">
+            <p>Columbia Missouri-Saint Louis Missouri</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+		    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		  </div>
+        </div>
+	    <div class="col-sm-3">
+           <div class="well">
+				<p>Flight Details:</p>
+				<!--<img src="paris.jpg" alt="Paris" width="400" height="300"> -->
+					<p><span class="glyphicon glyphicon-calendar"></span> July 21st</p>
+					<p><span class="glyphicon glyphicon-tag"></span> 3 bag limit</p>
+					<p><span class="glyphicon glyphicon-time"></span> 6hr flight</p>
+					<p><span class="glyphicon glyphicon-usd"></span> 299.99</p>
+				<button class="btn btn-primary">Book Now</button>
+		    </div>
+        </div>
+      </div>
+
+    </div>
+    <div class="col-sm-2 well">
+      <div class="thumbnail">
+      <div class="well">
+        <p>Text</p>
+      </div>
+      <div class="well">
+        <p>Text</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
+
